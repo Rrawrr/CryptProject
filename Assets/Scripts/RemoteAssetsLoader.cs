26feb3d9d28149d3be2f57b184727417be608d5a
Rrawrr <l0sty@yandex.ru>
2021-03-25 16:32:07 +0300

    RemoteAssetsLoader script added

---
+++ Assets/Scripts/RemoteAssetsLoader.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+
+public class RemoteAssetsLoader : MonoBehaviour
+{
+    [SerializeField] Transform parent;
+    [SerializeField] List<string> keys;
+
+    private void Awake()
+    {
+        keys = new List<string>();
+        keys.Add("ImageBear");
+        keys.Add("ImageLight");
+        keys.Add("ImageDark");
+    }
+
+    public void LoadAssets()
+    {
+        StartCoroutine(LoadAssetsCoroutine());
+    }
+
+    private IEnumerator LoadAssetsCoroutine()
+    {
+        AsyncOperationHandle<long> getDownloadSize = Addressables.GetDownloadSizeAsync(keys);
+        yield return getDownloadSize;
+        Debug.Log("Download size is: " + getDownloadSize.Result.ToString());
+
+        if (getDownloadSize.Result > 0)
+        {
+            AsyncOperationHandle downloadDependencies = Addressables.DownloadDependenciesAsync(keys[0]);
+            while (!downloadDependencies.IsDone)
+            {
+                Debug.Log("Downloadnig assets... " + downloadDependencies.PercentComplete);
+                yield return null;
+                if (getDownloadSize.Status == AsyncOperationStatus.Failed)
+                {
+                    Debug.LogError("Failed to download remote assets");
+                    break; 
+                }
+            }
+            downloadDependencies.Completed += ShowCompleteMessage;
+            downloadDependencies.Completed += InstantiatePrefabs;
+        }
+        else
+        {
+            InstantiatePrefabs();
+        }
+    }
+
+    private void ShowCompleteMessage(AsyncOperationHandle handle)
+    {
+        Debug.Log("Complete downloading assets");
+    }
+
+    private void InstantiatePrefabs(AsyncOperationHandle handle)
+    {
+        foreach (string key in keys)
+        {
+            Addressables.InstantiateAsync(key, parent);
+            Debug.Log("Instantiated " + key);
+        }
+    }
+
+    private void InstantiatePrefabs()
+    {
+        foreach (string key in keys)
+        {
+            Addressables.InstantiateAsync(key, parent);
+            Debug.Log("Instantiated " + key);
+        }
+    }
+
+
+}
